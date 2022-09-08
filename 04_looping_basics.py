79bf14f657a33df4ffae8ba5c4dd019898c8f108
Dohertyl1127 <111251142+dohertyl1127@users.noreply.github.com>
2022-09-08 12:14:51 +1200

    Add files via upload

--- 04_looping_basics.py
+++ 04_looping_basics.py
@@ -4,6 +4,10 @@ while not valid:
     response = float(input("Enter a number: ")) 
 
     if response > 0: 
-        valid = True
+        valid = True 
+    
+    else: 
+        print("Please enter a number ths is more than 0") 
+        print()
 
     
